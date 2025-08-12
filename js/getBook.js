f30014c007b91e20b7275c1479ae8de7ea04b21c
Nuth Chanrithea <nuthchanrithea654@gmail.com>
2025-08-13 01:15:48 +0700

    first commit

--- js/getBook.js
+++ js/getBook.js
@@ -1,5 +1,14 @@
 import { getData } from "../store/fetchData.js";
+import { cardBookComponent } from "../components/cardBookComponent.js";
 
-const book = await getData("books")
+const response = await getData("new");
 
-console.log(book)
+
+    const books = response.books;
+    const render_book = document.getElementById("book");
+    
+    books.map((book) => {
+        render_book.innerHTML += cardBookComponent(book);
+    });
+    
+    console.log(`Successfully rendered ${books.length} books`);
--- store/fetchData.js
+++ store/fetchData.js
@@ -3,8 +3,8 @@ const BASE_URL = "https://api.itbook.store/1.0/"
 
 export async function getData (endpoint) {
     const res = await fetch(BASE_URL + endpoint);
-    const data = res.json()
+    const data = await res.json()
+    console.log(data);
     return data
-    // console.log(data);
     
 }
