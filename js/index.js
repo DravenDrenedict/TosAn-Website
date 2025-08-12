9dc6386c9cf7a8c27ebe6902bb1c3ec32168edce
Nuth Chanrithea <nuthchanrithea654@gmail.com>
2025-08-13 01:44:16 +0700

    Remove all files from GitHub repo but keep local files

--- js/index.js
+++
@@ -1,9 +0,0 @@
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
-document.addEventListener('DOMContentLoaded', () => {
-  AOS.init({
-    duration: 1200,  // Customize animation duration
-    once: true,  // Only animate the element once
-  });
-});
